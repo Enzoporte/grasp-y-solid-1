268c036045835c027802d6977deb0eeccaa498ba
Enzoro <enzocantoni03@gmail.com>
2022-09-04 21:35:59 -0300

    Parte 1 - Expert GetProductionCost()

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -25,6 +25,23 @@ namespace Full_GRASP_And_SOLID.Library
             this.steps.Remove(step);
         }
 
+        // - Expert -
+        // Creo la responsabilidad solicitada dentro de Recipe ya que es la EXPERTA en información en este caso. 
+
+        public double GetProductionCost()
+        {
+            double costoInsumos = 0;
+            double costoEquipamiento = 0;
+            foreach (Step step in steps)
+            {
+                costoInsumos += step.Quantity * step.Input.UnitCost;
+                costoEquipamiento += step.Time * step.Equipment.HourlyCost;
+            }
+
+            double total = costoInsumos + costoEquipamiento;
+            return total;
+        }
+
         public void PrintRecipe()
         {
             Console.WriteLine($"Receta de {this.FinalProduct.Description}:");
@@ -33,6 +50,7 @@ namespace Full_GRASP_And_SOLID.Library
                 Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
                     $"usando '{step.Equipment.Description}' durante {step.Time}");
             }
+            Console.WriteLine($"\nCosto Total de Producción: {GetProductionCost().ToString()}");
         }
     }
 }
